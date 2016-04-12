858fcb4ed54e0cac943c0a57b559da3f5e80b2ec
xiaoliang.zxl <xiaoliang.zxl@taobao.com>
2016-04-12 17:37:36 +0800

    change title

--- sundaysearch/test.c
+++ sundaysearch/test.c
@@ -5,28 +5,34 @@
 void main(int argc,char* argv[]){
   int i;
   //字符串测试
-  char* src="hoverlee hehe xixi asdfasdfadfasdfashoverleesdi1294871-2alsdkjfzafsd hoverlees";
-  char* sub="hoverlees";
-  char* r=sunday_search(src,-1,sub,-1);
+  char* src = "hoverlee hehe xixi asdfasdfadfasdfashoverleesdi1294871-2alsdkjfzafsd hoverlees";
+  char* sub = "hoverlees";
+  char* r = sunday_search(src,-1,sub,-1);
   if(r) {
       printf("%s\n",r);
       printf("%d\n", r-src);
   }
-  else printf("not found\n");
+  else
+      printf("not found\n");
+  
   //内存块测试
-  /* src=(char*) malloc(8196000);
-     srand(1234567);
-     for(i=0;i<8196000;i++){
-     src[i]=rand()%256;
-     }
-     sub=(char*) malloc(1024000);
-     for(i=0;i<1024000;i++){
-     sub[i]=src[1234567+i];
-     }
-     r=sunday_search(src,8196000,sub,1024000);
-     if(r) printf("%d\n",r-src);
-     else printf("not found\n");
-     free(src);
-     free(sub);                                   */
+  src = (char*) malloc(8196000);
+ 
+  srand(1234567);
+  for(i = 0;i < 8196000; i++){
+      src[i] = rand()%256;
+  }
+  sub = (char*) malloc(1024000);
+  for(i = 0; i < 1024000; i++){
+      sub[i] = src[1234567+i];
+  }
+  printf("%s\n, %s\n", src, sub);
+  r = sunday_search(src,8196000,sub,1024000);
+  if (r)
+      printf("%d\n",r-src);
+  else
+      printf("not found\n");
+  free(src);
+  free(sub);
 
 }
